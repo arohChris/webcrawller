36c03e8bbd8c51d4b69a60fd22ab5275e8e00094
marknnes <nnennamark01@gmail.com>
2023-10-09 07:24:53 +0100

    get url from html and tests

--- crawl.js
+++ crawl.js
@@ -1,6 +1,38 @@
-function normaliseUrl(urlString) {
+ const { JSDOM } = require('jsdom')
+function getURLsFromHTML(htmlBody, baseURL) {
+
+    const URLs = [];
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    console.log(linkElements)
+    for (const linkElement of linkElements) {
+     if (linkElement.href.slice(0,1) === '/') {
+        //relative url
+      try {
+        const URLObj = new URL(`${baseURL}${linkElement.href}`)
+        URLs.push(URLObj.href)
+      } catch (err) {
+        console.log(`error with relative url: ${err.message}`)
+      }
+     
+      } else {
+        try {
+          const URLObj = new URL(linkElement.href)
+          URLs.push(URLObj.href)
+        } catch (err) {
+          console.log(`error with absolute url: ${err.message}`)
+        }
+       }
+      
+      
+    }
+    return URLs
+  
+}
+
+function normaliseURL(urlString) {
        const urlObj = new URL(urlString)
-       console.log(urlObj)
+   
       const hostPath = `${urlObj.hostname}${urlObj.pathname}`
       if (hostPath.length > 0 && hostPath.slice( -1 ) === `/`) {
 
@@ -14,6 +46,7 @@ function normaliseUrl(urlString) {
 }
 
 module.exports = {
-    normaliseUrl
+    normaliseURL,
+    getURLsFromHTML
 }
 
--- crawl.test.js
+++ crawl.test.js
@@ -1,24 +1,80 @@
-const { normaliseUrl } = require('./crawl.js');
+const { normaliseURL, getURLsFromHTML } = require('./crawl.js');
 
 const { test, expect } =require('@jest/globals')
 
 test('normaliseUrl strip protocol', () => {
     const input = 'https://blogs.boot.dev/path';
-    const actual = normaliseUrl(input);
+    const actual = normaliseURL(input);
     const expected = 'blogs.boot.dev/path';
     expect(actual).toEqual(expected)
 })
 
 test('normaliseUrl strip trailling slash ', () => {
     const input = 'https://blogs.boot.dev/path/';
-    const actual = normaliseUrl(input);
+    const actual = normaliseURL(input);
     const expected = 'blogs.boot.dev/path';
     expect(actual).toEqual(expected)
 })
 
 test('normaliseUrl strip http', () => {
     const input = 'https://blogs.boot.dev/path/where/';
-    const actual = normaliseUrl(input);
+    const actual = normaliseURL(input);
     const expected = 'blogs.boot.dev/path/where';
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML absolute', () => {
+    const inputHTMLBody = `
+    <html>
+         <body>
+             <div>
+                 <a href ="https://blogs.boot.dev/path/"> boot.devb blog path one </a>
+             </div>
+
+         </body>
+    </html>
+    `;
+    const inputBaseUrl = 'https://blogs.boot.dev'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseUrl);
+    const expected = ['https://blogs.boot.dev/path/'];
+    expect(actual).toEqual(expected)
+})
+
+ test('getURLsFromHTML relative', () => {
+    const inputHTMLBody = `
+
+    <html>
+         <body>
+             <div>
+                 <a href ="https://blogs.boot.dev/path1/"> boot.devb blog path one </a>
+             </div>
+             <div>
+                 <a href ="/path2/"> boot.devb blog path two </a>
+             </div>
+         </body>
+    </html>
+    `;
+
+    
+    const inputBaseUrl = 'https://blogs.boot.dev'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseUrl);
+    const expected = ['https://blogs.boot.dev/path1/','https://blogs.boot.dev/path2/'];
+    expect(actual).toEqual(expected)
+}) 
+
+test('getURLsFromHTML invalid', () => {
+    const inputHTMLBody = `
+    <html>
+         <body>
+             <div>
+                 <a href ="invalid"> invalid </a>
+             </div>
+
+         </body>
+    </html>
+    `;
+    const inputBaseUrl = 'https://blogs.boot.dev'
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseUrl);
+    const expected = [];
     expect(actual).toEqual(expected)
 })
